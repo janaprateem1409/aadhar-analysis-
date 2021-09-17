dff5a47f12970aeef4f1c12bf6dc103af2038424
Prateem Jana <59835038+janaprateem1409@users.noreply.github.com>
2021-09-17 10:52:33 +0530

    Update Sort_Reducer.java

--- hadoop/Sort_Reducer.java
+++ hadoop/Sort_Reducer.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class SortReducer extends Reducer<IntWritable, Text, Text, IntWritable> {
+public class Sort_Reducer extends Reducer<IntWritable, Text, Text, IntWritable> {
 
 	public void reduce(IntWritable key, Iterable<Text> values, Context context)
 			throws IOException, InterruptedException {
